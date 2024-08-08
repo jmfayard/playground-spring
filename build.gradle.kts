b1d5d35a8e5c0a53ffd5ddbc769b2b99e44e960d
Jean-Michel Fayard <jmfayard@gmail.com>
2024-08-08 13:06:43 +0200

    Bootstrap project with Spring Initializr

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.6"
+	id("com.netflix.dgs.codegen") version "6.2.1"
+}
+
+group = "dev.jmfayard"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["netflixDgsVersion"] = "9.0.0"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-graphql")
+	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-db2")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.ibm.db2:jcc")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework:spring-webflux")
+	testImplementation("org.springframework.graphql:spring-graphql-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:${property("netflixDgsVersion")}")
+	}
+}
+
+tasks.generateJava {
+	schemaPaths.add("${projectDir}/src/main/resources/graphql-client")
+	packageName = "dev.jmfayard.spring.codegen"
+	generateClient = true
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
