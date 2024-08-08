0acfce9b92efb0a73a894eb1650bf4c186b380e6
Jean-Michel Fayard <jmfayard@gmail.com>
2024-08-08 13:09:41 +0200

    I will do flyway & graphql maybe / later

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
-	id("com.netflix.dgs.codegen") version "6.2.1"
+	// id("com.netflix.dgs.codegen") version "6.2.1"
 }
 
 group = "dev.jmfayard"
@@ -30,9 +30,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-graphql")
-	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.flywaydb:flyway-database-db2")
+	// implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
+	// implementation("org.flywaydb:flyway-core")
+	// implementation("org.flywaydb:flyway-database-db2")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("com.h2database:h2")
@@ -50,11 +50,13 @@ dependencyManagement {
 	}
 }
 
+/**
 tasks.generateJava {
 	schemaPaths.add("${projectDir}/src/main/resources/graphql-client")
 	packageName = "dev.jmfayard.spring.codegen"
 	generateClient = true
 }
+/**
 
 tasks.withType<Test> {
 	useJUnitPlatform()
