a754c04a298ad98000ee48719f3ba04ae385833f
Jean-Michel Fayard <jmfayard@gmail.com>
2024-08-09 15:25:41 +0200

    Remove "magical" @RepositoryRestResource

--- src/main/java/dev/jmfayard/spring/InsurancePolicyRepository.java
+++ src/main/java/dev/jmfayard/spring/InsurancePolicyRepository.java
@@ -1,14 +1,7 @@
 package dev.jmfayard.spring;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.CrudRepository;
 
-@RepositoryRestResource(collectionResourceRel = "policies", path = "policies")
-public interface InsurancePolicyRepository extends PagingAndSortingRepository<InsurancePolicy, Long> {
-    InsurancePolicy save(InsurancePolicy policy);
-
-    void deleteById(Long id);
-
-    InsurancePolicy findById(Long id);
+public interface InsurancePolicyRepository extends CrudRepository<InsurancePolicy, Long> {
 
 }
